a9e0bc94161925f0935300e5ab5e167ff7d4cde3
Jay Liu <jayc.liu@gmail.com>
2011-08-24 15:19:35 -0700

    added page and node templates; altered page css for a more consistent display for now

--- sites/default/themes/computing/templates/page.tpl.php
+++ sites/default/themes/computing/templates/page.tpl.php
@@ -122,7 +122,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <!--<h1 class="title" id="page-title"> --><h2><?php print $title; ?></h2>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
@@ -134,7 +134,7 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+      <?php // print $feed_icons; ?> <!-- removed rss feed icon -->
     </div></div><!-- /.section, /#content -->
 
     <?php if ($page['navigation'] || $main_menu): ?>
